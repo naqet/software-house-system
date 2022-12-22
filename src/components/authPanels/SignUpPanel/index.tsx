daae5f9ddb59614b0233421596adc967ae75dadb
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2022-12-22 10:17:23 +0100

    feat(credentialsSignUp): Add sign up logic

--- src/components/authPanels/SignUpPanel/index.tsx
+++ src/components/authPanels/SignUpPanel/index.tsx
@@ -1,28 +1,119 @@
-import { type FormEvent, useState } from "react";
+import { useRouter } from "next/router";
+import { type FormEvent, useState, useRef, useReducer } from "react";
 import { FiEye, FiEyeOff, FiLoader } from "react-icons/fi";
+import { ZodError, type ZodIssue } from "zod";
+
+type Errors = {
+  [key: string]: string;
+};
+
+type ErrorsAction =
+  | {
+      type: "SET_GENERAL_ERROR";
+      payload: string;
+    }
+  | {
+      type: "SET_INPUT_ERRORS";
+      payload: ZodIssue[];
+    }
+  | {
+      type: "CLEAR_ERRORS";
+    };
+
+const reducer = (state: Errors, action: ErrorsAction) => {
+  switch (action.type) {
+    case "SET_GENERAL_ERROR":
+      return { generalError: action.payload };
+    case "SET_INPUT_ERRORS":
+      return action.payload.reduce(
+        (allErrors: Errors, currentError) => ({
+          ...allErrors,
+          ...{ [currentError.path[0] ?? "generalError"]: currentError.message },
+        }),
+        {}
+      );
+    case "CLEAR_ERRORS":
+      return {};
+    default:
+      return state;
+  }
+};
 
 export default function SignUpPanel() {
+  const router = useRouter();
+  const formRef = useRef<HTMLFormElement>(null);
   const [passType, setPassType] = useState<"text" | "password">("password");
   const [loading, setLoading] = useState(false);
+  const [errors, dispatch] = useReducer(reducer, {});
 
   const handlePasswordType = (): void => {
     setPassType((prev) => (prev === "text" ? "password" : "text"));
   };
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setLoading(true);
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      if (!formRef.current) throw new Error("Form is not valid");
+      setLoading(true);
+
+      const data = Object.fromEntries(new FormData(formRef.current));
+
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      // If it's a Bad Request, we need to handle input errors
+      if (response.status === 400) {
+        const error = (await response.json()) as ZodIssue[];
+
+        if (error && Array.isArray(error)) throw new ZodError(error);
+        // If it's any other error, we handle it.
+      } else if (400 < response.status && response.status < 600) {
+        const errorText = await response.text();
+        throw new Error(errorText ?? response.statusText);
+      }
+
+      // If redirect is happening, we need to handle it manually
+      if (response.redirected) {
+        router.push(response.url);
+        return;
+      }
+    } catch (e) {
+      setLoading(false);
+      if (e instanceof ZodError) {
+        dispatch({ type: "SET_INPUT_ERRORS", payload: e.errors });
+      } else if (e instanceof Error) {
+        dispatch({ type: "SET_GENERAL_ERROR", payload: e.message });
+      } else if (typeof e === "string") {
+        dispatch({ type: "SET_GENERAL_ERROR", payload: e });
+      }
+    }
   };
   return (
     <section className="w-[90%] max-w-md">
       <h1 className="mb-2 text-center text-2xl font-semibold">Join the team</h1>
       <section className="grid gap-2 rounded-lg border-1 p-4 dark:border-slate-700">
-        <form className="grid gap-4" onSubmit={handleSubmit}>
+        <form className="grid gap-4" onSubmit={handleSubmit} ref={formRef}>
           <div className="grid">
             <label htmlFor="name" className="ml-3 text-sm dark:text-slate-400">
               Name
             </label>
-            <input name="name" id="name" className="auth-input" required />
+            <input
+              name="name"
+              id="name"
+              className="auth-input"
+              required
+              aria-errormessage="nameError"
+            />
+            <span
+              id="nameError"
+              data-visible={!!errors.name}
+              className="error--info"
+            >
+              {errors.name}
+            </span>
           </div>
           <div className="grid">
             <label htmlFor="email" className="ml-3 text-sm dark:text-slate-400">
@@ -34,7 +125,15 @@ export default function SignUpPanel() {
               className="auth-input"
               type="email"
               required
+              aria-errormessage="nameError"
             />
+            <span
+              id="emailError"
+              data-visible={!!errors.email}
+              className="error--info"
+            >
+              {errors.email}
+            </span>
           </div>
           <div className="grid">
             <label
@@ -51,6 +150,7 @@ export default function SignUpPanel() {
                 required
                 minLength={8}
                 type={passType}
+                aria-errormessage="passwordError"
               />
               <button
                 type="button"
@@ -61,6 +161,13 @@ export default function SignUpPanel() {
                 {passType === "password" ? <FiEye /> : <FiEyeOff />}
               </button>
             </div>
+            <span
+              id="passwordError"
+              data-visible={!!errors.password}
+              className="error--info"
+            >
+              {errors.password}
+            </span>
           </div>
           <div className="grid">
             <label
@@ -75,7 +182,15 @@ export default function SignUpPanel() {
               className="auth-input w-full"
               required
               type={passType}
+              aria-errormessage="confirmPasswordError"
             />
+            <span
+              id="confirmPasswordError"
+              data-visible={!!errors.confirmPassword}
+              className="error--info"
+            >
+              {errors.confirmPassword}
+            </span>
           </div>
           <button
             type="submit"
@@ -90,6 +205,13 @@ export default function SignUpPanel() {
             )}
           </button>
         </form>
+        <span
+          id="generalError"
+          data-visible={!!errors.generalError}
+          className="error--info"
+        >
+          {errors.generalError}
+        </span>
       </section>
     </section>
   );
