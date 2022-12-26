04ed44dd9fb69bf9328fba6cce777c4fb5b8d5c4
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2022-12-26 21:20:14 +0100

    feat(settings): Add schema

--- src/components/main/SettingsPanel/UserSettings/index.tsx
+++ src/components/main/SettingsPanel/UserSettings/index.tsx
@@ -1,8 +1,15 @@
+import { type FormEvent, useRef } from "react";
+
 export default function UserSettings() {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
   return (
     <section className="grid w-full gap-4 rounded-lg border-1 p-4 @container dark:border-slate-800">
       <h2 className="font-semibold dark:text-slate-400">User</h2>
-      <form onSubmit={(e) => e.preventDefault()} className="grid gap-2">
+      <form onSubmit={handleSubmit} className="grid gap-2" ref={formRef}>
         <div className="grid gap-1">
           <label htmlFor="name" className="ml-3 text-sm dark:text-slate-400">
             Name
