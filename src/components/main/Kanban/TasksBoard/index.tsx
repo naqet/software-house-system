2b43d6c5ce996a9910667b735abd6b37eff5f673
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2023-01-03 14:55:57 +0100

    Delete kanban router

--- src/components/main/Kanban/TasksBoard/index.tsx
+++ src/components/main/Kanban/TasksBoard/index.tsx
@@ -1,45 +1,14 @@
-import type { Task } from "@prisma/client";
 import { useRouter } from "next/router";
-import { useMemo } from "react";
-import { trpc } from "../../../../utils/trpc";
-import KanbanColumn from "../../../shared/KanbanColumn";
-import Loader from "../../../shared/Loader";
 
 export default function TasksBoard() {
   const router = useRouter();
   const { projectId } = router.query;
-
-  const { isLoading, isError, data } = trpc.kanban.getTasks.useQuery(
-    typeof projectId === "string" ? projectId : ""
-  );
-
-  const tasks = useMemo(() => {
-    if (!data?.[0]) return [];
-
-    return [
-      ...new Set(([] as Task[]).concat(...data[0].map((epic) => epic.tasks))),
-    ];
-  }, [data]);
-
-  if (isLoading) return <Loader />;
-
-  if (isError) return <Loader />;
-
-  const [, statuses] = data;
 
   return (
     <ul
       id="js-kanban-board"
       // data-list-view is added/removed programmatically in KanbanViewSettings
       className="group flex w-full snap-x snap-mandatory flex-nowrap gap-4 overflow-y-scroll dark:data-[list-view=true]:flex-col"
-    >
-      {statuses.map((status) => (
-        <KanbanColumn
-          key={status.id}
-          name={status.name}
-          data={tasks.filter((task) => task.taskStatusId === status.id)}
-        />
-      ))}
-    </ul>
+    ></ul>
   );
 }
--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,13 +1,11 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { kanbanRouter } from "./kanban";
 import { projectRouter } from "./project";
 import { userRouter } from "./user";
 
 export const appRouter = router({
   project: projectRouter,
   auth: authRouter,
-  kanban: kanbanRouter,
   user: userRouter,
 });
 
