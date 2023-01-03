edc9cf229f71b946fbb3ebaf301d4f34c51e2f77
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2023-01-03 14:53:38 +0100

    refactor(kanban): Delete stories and epics

--- src/components/main/Kanban/index.tsx
+++ src/components/main/Kanban/index.tsx
@@ -1,25 +1,14 @@
-import { useRouter } from "next/router";
 import KanbanSettings from "./KanbanSettings";
-import StoriesBoard from "./StoriesBoard";
-import EpicsBoard from "./EpicsBoard";
 import TasksBoard from "./TasksBoard";
 
 export default function Kanban() {
-  const router = useRouter();
-  const { mode } = router.query;
   return (
     <main className="grid h-fit w-full gap-4 p-4 align-top">
       <div className="flex h-fit items-center justify-between">
         <h1 className="text-2xl">Project</h1>
         <KanbanSettings />
       </div>
-      {mode === "stories" ? (
-        <StoriesBoard />
-      ) : mode === "epics" ? (
-        <EpicsBoard />
-      ) : (
-        <TasksBoard />
-      )}
+      <TasksBoard />
     </main>
   );
 }
